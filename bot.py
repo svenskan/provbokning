bdf81b24dffa7653539ce5b053ae7acc41998be8
Ivan Ukhov <ivan.ukhov@gmail.com>
2022-05-27 19:26:33 +0200

    Simplify the logic

--- bot.py
+++ bot.py
@@ -21,6 +21,7 @@ class SeleniumDriver():
         self.driver.get('https://fp.trafikverket.se/boka/#/licence')
         self.driver.implicitly_wait(0.1)
         self.continue_running = True
+
     def select_exam(self):
         try:
             while self.driver.title == "Förarprov - Bokning":
@@ -30,6 +31,7 @@ class SeleniumDriver():
                 self.driver.find_element(By.XPATH, f"//*[@title='{config.license_type}']").click()
         except:
             pass
+
     def select_exam_type(self):
         try:
             exam_element = WebDriverWait(self.driver, 10).until(
@@ -37,6 +39,7 @@ class SeleniumDriver():
             ).click()
         except:
             pass
+
     def select_rent_or_language(self):
         try:
             WebDriverWait(self.driver, 10).until(
@@ -44,6 +47,7 @@ class SeleniumDriver():
             ).click()
         except:
             pass
+
     def select_location(self, location):
         self.location = location
         try:
@@ -54,29 +58,27 @@ class SeleniumDriver():
             location_element.send_keys(self.location, Keys.ENTER)
         except:
             pass
+
     def select_time(self, first_date, last_date):
-        self.first_date = first_date
-        self.last_date = last_date
+        first_date = datetime.strptime(first_date, '%Y-%m-%d').date()
+        last_date = datetime.strptime(last_date, '%Y-%m-%d').date()
         try:
             WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.XPATH, f"//*[text()='Lediga provtider']"))
             )
-            self.first_date = datetime.strptime(self.first_date, '%Y-%m-%d').date()
-            self.last_date = datetime.strptime(self.last_date, '%Y-%m-%d').date()
-            while self.first_date < self.last_date:
+            while first_date < last_date:
                 try:
-                    find_date = self.driver.find_element(By.XPATH, f"//*[contains(text(), '{str(self.first_date)}')]")
+                    find_date = self.driver.find_element(By.XPATH, f"//*[contains(text(), '{str(first_date)}')]")
                     if find_date:
                         find_date.find_element(By.XPATH, f"//*[text()='Välj']").click()
                         return False
                 except:
                     pass
-                finally:
-                    if self.first_date <= self.last_date:
-                        self.first_date = self.first_date + timedelta(days=1)
+                first_date = first_date + timedelta(days=1)
         except:
             pass
         return True
+
     def refresh_page(self):
         return self.driver.refresh()
 
@@ -90,23 +92,32 @@ def find_exam(driver, period=[60, 5 * 60, 1], speed=[5, 50, 0.1]):
             try:
                 driver.select_exam_type()
                 time.sleep(random.choice(speeds))
+                driver.select_location(i)
+                time.sleep(random.choice(speeds))
                 driver.select_rent_or_language()
                 time.sleep(random.choice(speeds))
-                if driver.continue_running:
-                    driver.select_location(i)
+                for j in range(0, len(config.dates), 2):
+                    driver.continue_running = driver.select_time(
+                        config.dates[j],
+                        config.dates[j + 1],
+                    )
+                    if not driver.continue_running:
+                        break
                     time.sleep(random.choice(speeds))
-                    for j in range(0, len(config.dates), 2):
-                        driver.continue_running = driver.select_time(config.dates[j], config.dates[j+1])
-                        time.sleep(random.choice(speeds))
-                        if not driver.continue_running:
-                            timestamp = datetime.now() + timedelta(minutes=15)
-                            while datetime.now() < timestamp:
-                                playsound('sounds/alert.mp3')
-                            break
+            except KeyboardInterrupt:
+                return
             except:
                 pass
+            if not driver.continue_running:
+                break
+        if not driver.continue_running:
+            break
+        timeout = random.choice(periods)
+        print(f'Sleeping for {timeout} seconds...')
+        time.sleep(timeout)
         driver.refresh_page()
-        time.sleep(random.choice(periods))
+    while True:
+        playsound('sounds/alert.mp3')
 
 if __name__ == '__main__':
     find_exam(SeleniumDriver())
--- config.py
+++ config.py
@@ -12,7 +12,7 @@ rent_or_language = 'Manuell bil'
 
 # One or more date ranges where every odd entry is the start date, and every
 # event entry is the corresponding end date
-dates = ['2022-05-30', '2022-06-27']
+dates = ['2022-05-30', '2022-06-28']
 
 # One or more locations
 locations = ['Farsta', 'Järfälla', 'Sollentuna', 'Tullinge', 'Uppsala']
